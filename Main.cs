8e354c14e8497b4e55eaf9f5c74d7f845857bc53
Mabel <mae@weatherelectric.xyz>
2024-07-28 00:31:28 -0700

    Change version number

--- Main.cs
+++ Main.cs
@@ -7,9 +7,9 @@ public class Main : MelonMod
     internal const string Author = "SoulWithMae";
     internal const string Company = "Weather Electric";
 #if DEBUG
-    internal const string Version = "1.0.1-DEBUG";
+    internal const string Version = "1.1.0-DEBUG";
 #else
-    internal const string Version = "1.0.1";
+    internal const string Version = "1.1.0";
 #endif
     internal const string DownloadLink = "https://thunderstore.io/c/bonelab/p/SoulWithMae/WaterDynamics/";
 
